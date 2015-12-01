72f46acf25e109f206783133abedc8de04bc0bfc
Carlos Moreno <carlosandresmorenovelez@gmail.com>
2015-11-30 19:59:43 -0500

    Cambio de service en en login, ahora se usa $firebaseAuth de AngularFire y no $firebaseSimpleLogin.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Cotrola las rutas, es el nucleo de la aplicacion
 // 1. Creo el modulo router que hace uso del modulo ngRoute
-var router = angular.module('routerApp', ['ngRoute', 'firebase', 'forms'])
+var router = angular.module('routerApp', ['firebase', 'ngRoute', 'forms'])
 
 // Configuración de las rutas
 router.config(function($routeProvider) {
@@ -26,9 +26,9 @@ router.config(function($routeProvider) {
 // --------------------------------------- Controladores--------------------------------------
 // url de la BD en Firebase: rentas.firebaseIO.com 
 // 1. Controlador del Login
-router.controller('loginCtrl', function($scope, $firebaseSimpleLogin, $location) {
-	var firebase_object = new Firebase('https://rentas.firebaseIO.com')
-	var login_object = $firebaseSimpleLogin(firebase_object) // Objeto simplelogin proporcionado por firebase
+router.controller('loginCtrl', function($scope, $firebaseAuth, $location) {
+	var firebaseRef = new Firebase('https://rentas.firebaseIO.com')
+	var autenticacion = $firebaseAuth(firebaseRef) // Objeto firebaseAuth proporcionado por firebase
 	$scope.user = {}
 
 	//Funcion para logear Usuarios
@@ -36,57 +36,38 @@ router.controller('loginCtrl', function($scope, $firebaseSimpleLogin, $location)
 		e.preventDefault()
 		var username = $scope.user.email
 		var password = $scope.user.password
-		login_object.$login('password', {
-				email : username,
-				password: password
-			})
-			.then(function(user) {
-				// Acá vendra el flujo si el usuario se puedo autentificar con exito 
-				//$location.path('/dashboard')
-				console.log('Exito !!, has sido autentificado')
-			}, function(error) {
-				console.log('ERROR al autentificar')
-			});
+		autenticacion.$authWithPassword({
+			email : username,
+			password : password
+		}).then(function(datos_autenticacion) {
+			console.log("EXITO",datos_autenticacion)
+		}).catch(function(error) {
+			console.log("ERROR", error)
+		});
 
 	}
 
 	//funcion para autenticacion federada
 	$scope.auth_federada = function(red_social) {
 				
-		firebase_object.authWithOAuthPopup(red_social, function (error, authData) {
-		// body...
-			if (error) {
-				console.log("Error al autentificar con "+red_social)
-			}else{
-				console.log("Exito al autentificar con "+red_social+" ",authData);
-			}
-		})
+		autenticacion.$authWithOAuthPopup(red_social)
+			.then(function(datos_autenticacion) {
+				console.log("EXITO AL AUTENTICAR CON "+red_social+"",datos_autenticacion)
+			})
+			.catch(function(error) {
+				console.log("ERROR AL AUTENTICAR CON "+red_social+"",error)
+			})
 		
 	}
-
-	// // Funcion para logear con Twitter
-	// $scope.login_twitter = function() {
-	// 	firebase_object.authWithOAuthPopup("twitter", function (error, authData) {
-	// 		// body...
-	// 		if (error) {
-	// 			console.log("Error al autentificar con Twitter")
-	// 		}else{
-	// 			console.log("Exito al autentificar con Twitter", authData);
-	// 		}
-	// 	})
-	// }
-	// }
-
-	// // Funcion para logear con Google
-	// $scope.login_google = function() {
-
-	// }
 });
 
 //2. Controlador de registro de usuarios
-router.controller('registroCtrl', function($scope) {
-	$scope.hola_mundo = 'Hola Registro'
-	console.log($scope.hola_mundo)
+router.controller('registroCtrl', function($scope, $firebaseObject) {
+	var firebaseRef = new Firebase('https://rentas.firebaseIO.com')
+	$scope.usuarios_registrados = $firebaseObject(firebaseRef.child('usuarios'))//aca obtengo los usuarios que tengo en firebase
+	console.log('Usuarios Registrados',$scope.usuarios_registrados)
+	// $scope.hola_mundo = 'Hola Registro'
+	// console.log($scope.hola_mundo)
 	// controller-body
 });
 
