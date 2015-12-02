606fe69eeebdc863b54e5ae702d34c4697940f1f
Carlos Moreno <carlosandresmorenovelez@gmail.com>
2015-12-01 23:55:58 -0500

    Validación de forms de registro y login. Ya se crean nuevos usuarios

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Cotrola las rutas, es el nucleo de la aplicacion
 // 1. Creo el modulo router que hace uso del modulo ngRoute
-var router = angular.module('routerApp', ['firebase', 'ngRoute', 'forms'])
+var router = angular.module('routerApp', ['firebase', 'ngRoute','forms'])
 
 // Configuración de las rutas
 router.config(function($routeProvider) {
@@ -40,9 +40,9 @@ router.controller('loginCtrl', function($scope, $firebaseAuth, $location) {
 			email : username,
 			password : password
 		}).then(function(datos_autenticacion) {
-			console.log("EXITO",datos_autenticacion)
+			console.log("EXITO e el logueo",datos_autenticacion)
 		}).catch(function(error) {
-			console.log("ERROR", error)
+			console.log("ERROR en el logue", error)
 		});
 
 	}
@@ -62,13 +62,25 @@ router.controller('loginCtrl', function($scope, $firebaseAuth, $location) {
 });
 
 //2. Controlador de registro de usuarios
-router.controller('registroCtrl', function($scope, $firebaseObject) {
-	var firebaseRef = new Firebase('https://rentas.firebaseIO.com')
-	$scope.usuarios_registrados = $firebaseObject(firebaseRef.child('usuarios'))//aca obtengo los usuarios que tengo en firebase
-	console.log('Usuarios Registrados',$scope.usuarios_registrados)
+router.controller('registroCtrl', function($scope, $firebaseAuth) {
 	// $scope.hola_mundo = 'Hola Registro'
 	// console.log($scope.hola_mundo)
 	// controller-body
+	var firebaseRef = new Firebase('https://rentas.firebaseIO.com')
+	var registro = $firebaseAuth(firebaseRef) // Objeto firebaseAuth proporcionado por firebase
+	$scope.user_register = {}
+	console.log("password = "+$scope.user_register.password +" -- "+"password2 = "+$scope.user_register.password2)
+	$scope.crearUsuario = function(e) {
+		e.preventDefault() // Para que a pagina no se refresque cuando estemos en la gestion de registar un nuevo usuario, pueden generarse errores. 
+		registro.$createUser({
+			email: $scope.user_register.email,
+			password: $scope.user_register.password
+		}).then(function(data_users) {
+			console.log("EXITO, Usuario Creado con uid ",data_users.uid)
+		}).catch(function(error) {
+			console.log("ERROR al crear usuario :(",error)
+		});
+	};
 });
 
 //3. Controlador del Dashboard
