27b5d1f787a7ffc81a6d544cfca4cfb47184ac82
Carlos Moreno <carlosandresmorenovelez@gmail.com>
2015-12-17 11:34:38 -0500

    Establecido monto a pagar por la declaración. Otros detalles resueltos

--- factorias/NotiFactoria.js
+++ factorias/NotiFactoria.js
@@ -13,43 +13,55 @@ Cualquiera de las cuatros condiciones anteriores es suficiente para presentar la
 var rentgular = angular.module('rentgularApp')
 rentgular.factory('servicioNoti', [
 	function() {
-		var TOPE_INGRESOS = 38479000 //Ingresos que se deben superar para que declare renta
+		//Topes que se deben de superar para declarar renta
+		var TOPE_INGRESOS = 38479000 
 		var TOPE_COMPRAS = 76958000
 		var TOPE_PATRIMONIO = 123682500
 		var TOPE_TRANSACCIONES = 76958000
+		// monto a pagar si los topes son excedidos
+		var monto_pagar = 0
 		var obj = {
+			/**
+			 * [debe_declarar Esta funcion notifica al usario el monto a pagar dependiendo de unos
+			 * montos establecidos, hace uso del servicio notify para realizar las notificaciones]
+			 * @param  {[int]} total  [el total en cualquiera de sus variaciones: de ingresos, de compras, de transacciones y de patrimonio]
+			 * @param  {[service]} notify [un servicio externo para la gestión de notificaciones]
+			 * @param  {[string]} motivo [el motivo de causal]
+			 * @return {[void]}        [void]
+			 */
 			debe_declarar:	function(total, notify, motivo) {
 					if(motivo == "INGRESOS") {
-						console.log("Hola")
 						if(total > TOPE_INGRESOS){
-							notify({message:'Debes declarar renta, has excedido el monto de tus ingresos',
-									duration:'10000'
-
+							monto_pagar = total * 0.45	 
+							notify({message:'Has excedido el monto de tus ingresos, debes declarar renta por valor de $'+monto_pagar,
+									duration:'15000'
 							})
 						}	
 					}else if(motivo == "COMPRAS"){
 						if(total > TOPE_COMPRAS){
-							notify({message:'Debes declarar renta, has excedido el monto de tus compras',
-									duration:'10000'
+							monto_pagar = total * 0.25
+							notify({message:'Has excedido el monto de tus compras, debes declarar renta por valor de'+monto_pagar,
+									duration:'15000'
 							})
 						}
 					}else if(motivo == "PATRIMONIO"){
 						console.log(TOPE_PATRIMONIO)
 						if(total > TOPE_PATRIMONIO){
-							console.log('ENTRE AL SEG IF')
-							notify({message:'Debes declarar renta, has excedido el monto del patrimonio',
-									duration:'10000'
+							monto_pagar = total * 0.65
+							notify({message:'Has excedido el monto del patrimonio, debes declarar renta por valor de'+monto_pagar,
+									duration:'15000'
 							})
 							
 						}
 					}else if(motivo == "TRANSACCIONES"){
 						if(total > TOPE_TRANSACCIONES){
-							notify({message:'Debes declarar renta, has excedido el monto de transacciones',
-									duration:'10000'
+							monto_pagar = total * 0.3
+							notify({message:'Has excedido el monto de transacciones, debes declarar renta por valor de'+monto_pagar,
+									duration:'15000'
 							})	
 						}
 					}
-				}
-		}
+				}//fin debe_declarar
+		}//fin obj
 		return obj
 }])
